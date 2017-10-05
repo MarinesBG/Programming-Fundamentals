04c8c1ef37df72b502c9796a9c663e6006ff636a
MarinesBG <Demirev1994@gmail.com>
2017-10-05 11:11:04 +0300

    Upload three folder for Lab

---
+++ Programming-Fundamentals-Lab/04 - Methods Debugging - Lab/P09-Multiply Evens by Odds/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P09_Multiply_Evens_by_Odds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(GetMultipleOfEvensAndOdds(number));
+        }
+
+        static int GetMultipleOfEvensAndOdds(int number)
+        {
+            number = Math.Abs(number);
+            int evenDigitSum = GetSumOfOddOrEvenDigits(number, true);
+            int oddDigitSum = GetSumOfOddOrEvenDigits(number, false);
+            return evenDigitSum * oddDigitSum;
+        }
+
+        static int GetSumOfOddOrEvenDigits(int number, bool isEven)
+        {
+            int remainder = isEven ? 0 : 1;
+            int sum = 0;
+            while (number > 0)
+            {
+                int digit = number % 10;
+                if (digit % 2 == remainder)
+                {
+                    sum += digit;
+                }
+                number /= 10;
+            }
+            return sum;
+        }
+    }
+}
+        
