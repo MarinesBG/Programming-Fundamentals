54e0ac6b225041e05950fcfd007dc9d7f2c432bf
MarinesBG <Demirev1994@gmail.com>
2017-10-05 13:38:26 +0300

    Add one folder

---
+++ Programming-Fundamentals-Exercise/03 - Data Types and Variables - Exercise/P19-Thea the Photographer/Program.cs
@@ -0,0 +1,31 @@
+﻿
+namespace P19_Thea_the_Photographer
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            int numberOfPictures =int.Parse(Console.ReadLine());
+            double pictureFilterTime = double.Parse(Console.ReadLine());
+            double percentageOfApprovedPicture = double.Parse(Console.ReadLine());
+            double uploadTimeForPucture = double.Parse(Console.ReadLine());
+
+            double filteredPic = Math.Ceiling((numberOfPictures * percentageOfApprovedPicture / 100));
+            double filteredTime = numberOfPictures * pictureFilterTime;
+            double uploadedTime = filteredPic * uploadTimeForPucture;
+            double totalTime = filteredTime + uploadedTime;
+         
+            TimeSpan timeInSecond = TimeSpan.FromSeconds(totalTime);
+
+            string time = string.Format($"{timeInSecond.Days}:" +
+                                        $"{timeInSecond.Hours:D2}:" +
+                                        $"{timeInSecond.Minutes:D2}:" +
+                                        $"{timeInSecond.Seconds:D2}");
+
+           
+              Console.WriteLine(time);
+        }
+    }
+}
