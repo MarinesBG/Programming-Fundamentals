3d2c3d9168732d232e8c7514382f918f7a7e651f
MarinesBG <Demirev1994@gmail.com>
2017-10-25 21:40:21 +0300

    Upload Strings and Text PRocessing - LAb

---
+++ Programming-Fundamentals-Lab/10 - Strings and Text Processing - Lab/03. Text Filter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Text_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] badWords = Console.ReadLine()
+                .Split(new char[]{' ',',','.'} ,StringSplitOptions.RemoveEmptyEntries);
+            
+            string text = Console.ReadLine();
+
+            foreach (var badWord in badWords)
+            {
+                if (text.Contains(badWord))
+                {
+                    text = text.Replace(badWord, new string('*',badWord.Length));
+                }
+            }
+            Console.WriteLine(text);
+
+
+        }
+    }
+}
---
+++ Programming-Fundamentals-Lab/10 - Strings and Text Processing - Lab/04. Palindromes/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Palindromes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] text = Console.ReadLine()
+                .Split(new char[] { ' ', '.', ',', '!', '?', '/' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            List<string> palindrome = new List<string>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                bool palind = IsPalindorme(text[i]);
+                if (palind == true)
+                {
+                    palindrome.Add(text[i]);
+                }
+
+            }
+
+            Console.WriteLine(String.Join(", ", palindrome.OrderBy(x => x).Distinct()));
+        }
+
+        private static bool IsPalindorme(string text)
+        {
+
+            string reverseText = new string(text.Reverse().ToArray());
+
+            if (reverseText == text)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
+
