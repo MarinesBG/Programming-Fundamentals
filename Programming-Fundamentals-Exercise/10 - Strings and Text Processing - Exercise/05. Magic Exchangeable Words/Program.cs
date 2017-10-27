bf62f0436b365c9cfa7a6744589551f22fb6f52e
MarinesBG <Demirev1994@gmail.com>
2017-10-27 17:40:48 +0300

    Upload file in folder

---
+++ Programming-Fundamentals-Exercise/10 - Strings and Text Processing - Exercise/05. Magic Exchangeable Words/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Magic_Exchangeable_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            string first = input[0];
+            string second = input[1];
+
+            char[] firstWord = first.ToCharArray().Distinct().ToArray();
+            char[] secondWord = second.ToCharArray().Distinct().ToArray();
+
+
+            if (firstWord.Length == secondWord.Length)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+
+
+        }
+    }
+}
