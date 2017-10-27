4f4c61619c8f533188cf9f6f80f155490a8cf89d
MarinesBG <Demirev1994@gmail.com>
2017-10-27 17:38:49 +0300

    Upload file in folder

---
+++ Programming-Fundamentals-Exercise/10 - Strings and Text Processing - Exercise/01. Convert from Base-10 to Base-N/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Convert_from_Base_10_to_Base_N
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger[] input = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(BigInteger.Parse)
+                .ToArray();
+
+            List<BigInteger> result = new List<BigInteger>();
+
+            BigInteger divide = 0;
+
+
+            while (input[1] > 0)
+            {
+                divide = input[1] % input[0];
+
+                input[1] = input[1] / input[0];
+
+                result.Add(divide);
+            }
+            result.Reverse();
+            Console.WriteLine(String.Join("",result));
+        }
+    }
+}
