11ecb678598da075efd6583b0e9f69983f45b4c7
MarinesBG <Demirev1994@gmail.com>
2017-10-27 17:40:00 +0300

    Upload file in folder

---
+++ Programming-Fundamentals-Exercise/10 - Strings and Text Processing - Exercise/03. Unicode Characters/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Unicode_Characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] input = Console.ReadLine().ToCharArray();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int number = (int)input[i];
+                string hexValue = number.ToString("X");
+                Console.Write("\\u00{0}", hexValue.ToLower());
+            }
+            Console.WriteLine();
+        }
+    }
+}
